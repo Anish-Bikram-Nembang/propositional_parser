1d7a4031cbc4d320f472c8e7473288beab36dea5
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-09-09 12:50:53 +0545

    add print option

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "./evaluator/evaluator.hpp"
 #include "./lexer/lexer.hpp"
 #include "./postfixProducer/postfix.hpp"
+#include "CLI/CLI.hpp"
 #include "truthTableGenerator/truthTableGenerator.hpp"
 #include "varNamesCollector/varNamesCollector.hpp"
 #include <iostream>
@@ -11,56 +12,21 @@
 
 using namespace std;
 
-int main(void) {
-  string proposition;
-  int option;
-  cout << "select an option: " << '\n'
-       << "1. generate truth table 2.evaluate a specific expression path"
-       << '\n';
-  cin >> option;
-  cin.ignore();
-  switch (option) {
-  case 1: {
-    cout << "Enter a proposition:" << '\n';
-    getline(cin, proposition);
-    vector<Token> lexedProposition = lex(proposition);
-    vector<Token> postfixProposition = producePostfix(lexedProposition);
-    generateTruthTable(postfixProposition, collectVarNames(postfixProposition),
-                       proposition);
-    break;
-  }
-  case 2: {
-    cout << "Enter a proposition:" << '\n';
-    getline(cin, proposition);
-    vector<Token> lexedProposition = lex(proposition);
-    vector<Token> postfixProposition = producePostfix(lexedProposition);
-    vector<string> vars = collectVarNames(postfixProposition);
+int main(int argc, char **argv) {
+  CLI::App app{"a propositional parser"};
 
-    unordered_map<string, bool> varWithValues{};
-    cout << "Allowed values are:  \n 1. for truth values : T or t or 1\n 2. "
-            "for false values : F or f or 0\n";
-    for (const string &var : vars) {
-      string val{};
-      cout << "Enter value for " << var << ": ";
-      cin >> val;
-      if (val == "1" || val == "t" || val == "T") {
-        varWithValues.insert({var, true});
-      } else if (val == "0" || val == "f" || val == "F") {
-        varWithValues.insert({var, false});
-      } else {
-        throw invalid_argument("Invalid input");
-      }
-    }
-    cout << proposition << " : " << eval(postfixProposition, varWithValues)
-         << '\n';
+  app.require_subcommand(1);
 
-    break;
+  auto print = app.add_subcommand("print", "Print the truth table");
+  string propositionToPrint;
+  print->add_option("proposition", propositionToPrint,
+                    "proposition to display truth table of");
+  CLI11_PARSE(app, argc, argv);
+  if (*print) {
+    vector<Token> lexedProposition = lex(propositionToPrint);
+    vector<Token> postfixedProposition = producePostfix(lexedProposition);
+    vector<string> variables = collectVarNames(postfixedProposition);
+    generateTruthTable(postfixedProposition, variables, propositionToPrint);
   }
-  case 3:
-    break;
-  default:
-    break;
-  }
-
   return 0;
 }
