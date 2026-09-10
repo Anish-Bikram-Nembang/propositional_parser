a87e46737ac4d213d784f4f079b831a1b828e125
Anish-Bikram-Nembang <anish.bikram.nembang@gmail.com>
2026-09-10 12:25:39 +0545

    add truth table generation confirmation when no of variables exceed 8 and disallow more than 20 variables

--- src/truthTableGenerator/truthTableGenerator.cpp
+++ src/truthTableGenerator/truthTableGenerator.cpp
@@ -2,14 +2,30 @@
 #include "../evaluator/evaluator.hpp"
 #include <iomanip>
 #include <iostream>
+#include <stdexcept>
 #include <unordered_map>
 
 void generateTruthTable(const std::vector<Token> &postfix,
                         const std::vector<std::string> &variables,
                         const std::string &proposition) {
+  char option = 'y';
   std::unordered_map<std::string, bool> varValuesMap{};
+  if (variables.size() > 20) {
+    throw std::invalid_argument("Maximum no of variables supported is 20");
+  }
+  if (variables.size() > 8) {
+    std::cout << "This will generate  " << ((size_t)1 << variables.size())
+              << " rows in the truth"
+              << "table\nDo you want to continue? (y/n): ";
+    std::cin >> option;
+  }
+  if (option != 'y' && option != 'Y') {
+    std::cout << "Aborting truth table generation\n";
+    return;
+  }
+
   size_t truthTableWidth{0};
-  for (size_t assignment = 0; assignment < (1 << variables.size());
+  for (size_t assignment = 0; assignment < ((size_t)1 << variables.size());
        assignment++) {
     varValuesMap.clear();
     for (size_t i = 0; i < variables.size(); i++) {
